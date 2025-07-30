6496d9053e9561833873d08d42675649856e5d16
313 <baki.ozturk.2000@gmail.com>
2025-07-30 19:02:51 +0200

    Create walletManager.js

---
+++ src/walletManager.js
@@ -0,0 +1,8 @@
+// walletManager.js - Modulo disattivato
+// Questo modulo è stato disattivato temporaneamente. Nessuna funzionalità BTC attiva.
+
+module.exports = {
+  getBalance: async () => ({ address: "DISATTIVATO", balance: 0 }),
+  sendBTC: async () => "Modulo disattivato",
+  address: "DISATTIVATO"
+};
